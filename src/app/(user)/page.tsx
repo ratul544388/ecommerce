8620706b99a151f91529e72f1fc7e141ba7e88c2
ratul544388 <ratulhossainbnc@gmail.com>
2024-02-13 18:53:59 +0600

    final

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
     <div className="h-full space-y-4 pb-10">
       <Hero />
       <div className="h-full space-y-4 pt-[50vh]">
-        <MainCategories />
+        {/* <MainCategories /> */}
         <div className="max-w-screen-lg px-5 mx-auto space-y-5">
-          <NewArrival />
+          {/* <NewArrival /> */}
           <AboutOurCloth />
         </div>
       </div>
