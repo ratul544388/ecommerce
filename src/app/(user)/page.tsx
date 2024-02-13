75198507f0ef7c90eac4601f740b91f8214b5905
ratul544388 <ratulhossainbnc@gmail.com>
2024-02-13 18:57:19 +0600

    test 2

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,20 +1,11 @@
-import { Hero } from "@/components/hero";
-import { MainCategories } from "./_components/main-categories";
-import { NewArrival } from "./_components/new-arrival";
-import { AboutOurCloth } from "./_components/about-our-cloth";
-export const dynamic = "force-dynamic";
+import React from 'react'
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="h-full space-y-4 pb-10">
-      <Hero />
-      <div className="h-full space-y-4 pt-[50vh]">
-        {/* <MainCategories /> */}
-        <div className="max-w-screen-lg px-5 mx-auto space-y-5">
-          {/* <NewArrival /> */}
-          <AboutOurCloth />
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
+  )
 }
+
+export default HomePage
