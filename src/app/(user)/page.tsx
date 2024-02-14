b423d39018a1f0157f7fbaef05ee2de11f8801de
ratul544388 <ratulhossainbnc@gmail.com>
2024-02-14 23:00:31 +0600

    test

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -6,11 +6,11 @@ import { AboutOurCloth } from "./_components/about-our-cloth";
 export default function Home() {
   return (
     <div className="h-full space-y-4 pb-10">
-      <Hero />
+      {/* <Hero /> */}
       <div className="h-full space-y-4 pt-[50vh]">
         <MainCategories />
         <div className="max-w-screen-lg px-5 mx-auto space-y-5">
-          <NewArrival />
+          {/* <NewArrival /> */}
           <AboutOurCloth />
         </div>
       </div>
